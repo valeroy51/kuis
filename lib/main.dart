85b460e7bb3293bc2ae1064dca8e806588578e1e
Valeroy <valeroy51@gmail.com>
2023-11-15 15:18:43 +0700

    selesai ?

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,10 +10,121 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          leading: const Icon(Icons.arrow_back),
+          title: const TextField(
+            decoration: InputDecoration(
+                hintText: 'Search', suffixIcon: Icon(Icons.search)),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(CupertinoIcons.shopping_cart))
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Semua produk',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Promosi', style: TextStyle(fontSize: 17)),
+                Text('Nama Produk', style: TextStyle(fontSize: 17)),
+                Text('Terlaris', style: TextStyle(fontSize: 17)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      buildProductCard(
+                        'img/produk4.png',
+                        'Redmi Note 9',
+                        'Rp.5.000.000',
+                      ),
+                      buildProductCard(
+                        'img/produk3.png',
+                        'Baterai Alkalin AAA',
+                        'Rp.30.000',
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      buildProductCard(
+                        'img/produk2.png',
+                        'Sabun Lifebuoy',
+                        'Rp.3.000',
+                      ),
+                      buildProductCard(
+                        'img/produk1.png',
+                        'Minyak kita',
+                        '\Rp.5.000',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildProductCard(String imagePath, String productName, String price) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 1,
+      margin: const EdgeInsets.all(16.0),
+      child: Padding(
+        padding: EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage(imagePath),
+              width: 80,
+              height: 80,
+            ),
+            SizedBox(height: 16.0),
+            Text(productName, style: TextStyle(fontSize: 16)),
+            SizedBox(height: 16.0),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Text(price, style: TextStyle(fontSize: 14)),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                  child: Text(
+                    '+ Keranjang',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
